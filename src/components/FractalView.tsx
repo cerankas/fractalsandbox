0e14415def439309a2ef654f3a2f07567dc2d9f9
cerankas <cerankas@gmail.com>
2024-03-29 07:11:03 +0100

    fix missing FractalView redraws

--- src/components/FractalView.tsx
+++ src/components/FractalView.tsx
@@ -8,6 +8,7 @@ export default function FractalView(props: {size: number, fractal: string, color
   const canvasRef = useRef(null);
   const fracRef = useRef<FractalImageComputer | null>(null);
   const [drawStep, setDrawStep] = useState(0);
+  const [drawStart, setDrawStart] = useState(false);
 
   useEffect(() => {
     const canvas = canvasRef.current as unknown as HTMLCanvasElement;
@@ -19,28 +20,29 @@ export default function FractalView(props: {size: number, fractal: string, color
 
   useEffect(() => {
     fracRef.current?.setWidthHeight(props.size, props.size);
-    setDrawStep(0);
+    setDrawStart(true);
   }, [props.size]);
   
   useEffect(() => {
     fracRef.current?.setFormulas(Formula.formulasFromString(props.fractal));
-    setDrawStep(0);
+    setDrawStart(true);
   }, [props.fractal]);
   
   useEffect(() => {
     const palette = createPaletteFromKeys(paletteKeysFromString(props.color) as PaletteKey[]);
     fracRef.current?.setPalette(palette);
-    setDrawStep(0);
+    setDrawStart(true);
   }, [props.color]);
 
   useEffect(() => {
+    setDrawStart(false);
     let timerId: NodeJS.Timeout;
     fracRef.current!.processInBackground();
     if (!fracRef.current?.isFinished()) {
       timerId = setTimeout(() => setDrawStep(drawStep + 1), 1);
     }
     return () => clearTimeout(timerId);
-  }, [drawStep]);
+  }, [drawStep, drawStart]);
 
   return (
     <canvas ref={canvasRef} width={props.size} height={props.size}></canvas>
