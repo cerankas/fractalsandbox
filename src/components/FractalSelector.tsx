2896872c13941487bdb1057960cfdd426f4ee23f
cerankas <cerankas@gmail.com>
2024-07-17 10:58:42 +0200

    tiles resized by browser from original size (300) to any selected size [100-300]

--- src/components/FractalSelector.tsx
+++ src/components/FractalSelector.tsx
@@ -5,8 +5,7 @@ import { iconStyle } from "./browserUtils";
 import { useLocalStorage } from "./browserUtils";
 
 export default function FractalSelector(props: { fractals: Fractal[], onmousedown: (button: number, fractal: Fractal) => void, selected: number, menu: React.ReactNode, refreshCallback?: () => void }) {
-  const sizes = [50, 100, 150, 200, 250];
-  const [tileSize, setTileSize] = useLocalStorage('tileSize', '1');
+  const [tileSize, setTileSize] = useLocalStorage('tileSize', '300');
   return (
     <div className="flex flex-col size-full gap-2">
       <div className="flex flex-row bg-white justify-between">
@@ -14,8 +13,8 @@ export default function FractalSelector(props: { fractals: Fractal[], onmousedow
           <input
             className="w-[100px] ml-1 mr-1 accent-gray-500 cursor-pointer"
             type="range"
-            min={0}
-            max={sizes.length - 1}
+            min={100}
+            max={300}
             value={tileSize}
             onChange={(e) => { setTileSize(e.target.value); localStorage.tileSize = e.target.value; }}
             title="Tile size"
@@ -28,7 +27,7 @@ export default function FractalSelector(props: { fractals: Fractal[], onmousedow
         { props.fractals.map(((f) => <FractalTile 
           key={f.id}
           fractal={f}
-          size={sizes[parseInt(tileSize)] ?? 50} 
+          size={parseInt(tileSize) ?? 300} 
           onmousedown={props.onmousedown}
           selected={f.id == props.selected}
         />)) }
--- src/components/FractalTile.tsx
+++ src/components/FractalTile.tsx
@@ -22,16 +22,18 @@ export default function FractalTile(props: { fractal: Fractal, size: number, onm
 
   return (
     <div
-      className={`flex flex-grow justify-center size-[${props.size}px] box-border border-2 border-transparent hover:border-slate-200`}
+      className={`flex flex-grow justify-center box-border border-2 border-transparent hover:border-slate-200`}
       style={{
         backgroundColor: backgroundColor(props.fractal.color),
         borderColor: props.selected ? oppositeBackgroundColor(props.fractal.color) : '',
+        width: `${props.size + 4}px`,
+        height: `${props.size + 4}px`,
       }}
     >
       <canvas
         ref={canvasRef}
-        width={props.size}
-        height={props.size}
+        width={300}
+        height={300}
         onMouseDown={(e: React.MouseEvent) => {
           props.onmousedown(e.button, props.fractal);
           e.preventDefault();
