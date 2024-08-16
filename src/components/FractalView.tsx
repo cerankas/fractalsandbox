a9873307ec676da03e4b08711996685b3692e252
cerankas <cerankas@gmail.com>
2024-08-16 07:03:02 +0200

    remove render progress indicator

--- src/components/FractalView.tsx
+++ src/components/FractalView.tsx
@@ -1,13 +1,11 @@
-import React, { useEffect, useMemo, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef } from "react";
 import FractalRenderer from "~/math/fractalRenderer";
-import ProgressIndicator from "./ProgressIndicator";
-import { backgroundColor, oppositeBackgroundColor } from "~/math/palette";
+import { backgroundColor } from "~/math/palette";
 import { useResizeObserver } from "./browserUtils";
 
 export default function FractalView(props: { form: string, color: string, updateCanvasRef?: (canvas: HTMLCanvasElement) => void }) {
-  const [progress, setProgress] = useState(0);
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const renderer = useMemo(() => new FractalRenderer({initPriority: 0, drawPriority: 2, cached: true, onprogress: setProgress}), []);
+  const renderer = useMemo(() => new FractalRenderer({initPriority: 0, drawPriority: 2, cached: true}), []);
 
   useEffect(() => props.updateCanvasRef?.(canvasRef.current!), [props, props.updateCanvasRef])
 
@@ -24,10 +22,6 @@ export default function FractalView(props: { form: string, color: string, update
   return (
     <div className="size-full" style={{backgroundColor: backgroundColor(props.color)}}>
       
-      <div className="absolute top-0 left-0">
-        <ProgressIndicator progress={progress} color={oppositeBackgroundColor(props.color)}/>
-      </div>
-
       <canvas className="size-full"
         ref={canvasRef}
       />
