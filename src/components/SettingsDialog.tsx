d953d6e8f8e22b40bb5f8e1aa1a02ad670dc2d2d
cerankas <cerankas@gmail.com>
2024-08-18 06:56:01 +0200

    feat(settings): clear fractal cache

--- src/components/SettingsDialog.tsx
+++ src/components/SettingsDialog.tsx
@@ -11,7 +11,9 @@ export default function SettingsDialog(props: {slideShowPeriod: number, setSlide
     document.addEventListener('keydown', closeOnEscape, true);
     return () => document.removeEventListener('keydown', closeOnEscape, true);
   }, [props]);
-  
+
+  const buttonStyle = "border w-40 rounded-sm m-2"
+
   return <>
     <div className="fixed inset-0 z-40 bg-black opacity-50" onClick={props.close}/>
     <div className="fixed inset-24 flex flex-col rounded z-50 border-2 bg-white border-black p-2">
@@ -19,17 +21,25 @@ export default function SettingsDialog(props: {slideShowPeriod: number, setSlide
         className={iconStyle + " absolute right-0 top-0"}
         onClick={props.close}
       />
-      Settings (build {process.env.NEXT_PUBLIC_BUILD_TIMESTAMP?.slice(0,10)})<br/>
-      <br/>
+      Settings (build {process.env.NEXT_PUBLIC_BUILD_TIMESTAMP?.slice(0,10)})
       <label>
-        Slideshow period (in seconds): &nbsp;
+        Slideshow period (in seconds):
         <input
           type="number" min={1}
+          className={buttonStyle}
           style={{width:60}}
           value={props.slideShowPeriod / 1000}
           onChange={(e) => props.setSlideShowPeriod(parseFloat(e.target.value) * 1000)}
-        />
+          />
       </label>
+      <div className="flex flex-row">
+        <input
+          type="button"
+          value="Clear fractal cache"
+          className={buttonStyle}
+          onClick={() => { localStorage.removeItem("fractalCache"); location.reload(); }}
+        />
+      </div>
     </div>
   </>;
 }
